84eaa72d6d67b634027156bfe675fb2703fae93a
DiGergo <okos_gergo@yahoo.com>
2020-01-14 13:06:16 +0000

    Turned off debug mode

--- app.py
+++ app.py
@@ -125,4 +125,4 @@ def insert_recipes():
 if __name__ == '__main__' :
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
